322b5104ead57a6b211ae700095bdf7df886f28c
RBATOKS <91324313+RBATOKS@users.noreply.github.com>
2021-11-16 17:39:10 -0300

    Add files via upload

---
+++ CONSULTA_CEP_CORREIOS/Project1.dpr
@@ -0,0 +1,16 @@
+program Project1;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  ConsultaCEP in '..\ConsultaCEP.pas',
+  AtendeCliente1 in 'AtendeCliente1.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
